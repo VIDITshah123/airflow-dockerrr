611beecf624dbb0737e063bb42c2bc8c91716ea8
VIDITshah123 <shahvidit15@gmail.com>
2026-02-24 16:57:53 +0530

    ok

--- dags/plugin_test_dag.py
+++ dags/plugin_test_dag.py
@@ -21,7 +21,7 @@ def plugin_test():
 # query2
     result2 = db.select_statement(
     selector="SELECT",
-    column_name="file_id, file_name",
+    column_name="file_id, file_path, zip_flag, delete_flag",
     condition="file_id > 10"
     )
     print("Result 2:", result2)
@@ -31,15 +31,15 @@ def plugin_test():
     result3 = db.select_statement(
     selector="SELECT",
     column_name="*",
-    condition=None
     )
     print("Result 3:", result3)
+    print("------------------------")
 
 # query4
     result4 = db.select_statement(
     selector="SELECT",
-    column_name="file_name",
-    condition="1=1 LIMIT 5"
+    column_name="file_id, file_path, zip_flag, delete_flag",
+    condition="zip_flag = 'T' AND delete_flag = 'T'"
     )
     print("Result 4:", result4)
     print("------------------------")
@@ -47,8 +47,8 @@ def plugin_test():
 # query5
     result5 = db.select_statement(
     selector="UPDATE",
-    update_values="is_expired = 'F'",
-    condition="file_id = 1"
+    update_values="is_expired = 'F' AND deletion_attempted = 'F'",
+    condition="file_id = 33"
     )
     print("Rows Updated:", result5)
     print("------------------------")
@@ -56,8 +56,8 @@ def plugin_test():
 # query6
     result6 = db.select_statement(
     selector="UPDATE",
-    update_values="is_expired = 'T'",
-    condition="file_id > 50"
+    update_values="zip_flag = 'F' AND delete_flag = 'F'",
+    condition="file_id > 36"
     )
     print("Rows Updated:", result6)
     print("------------------------")
@@ -66,29 +66,21 @@ def plugin_test():
     result7 = db.select_statement(
     selector="DELETE",
     column_name="",
-    condition="file_id = 999"
+    condition="file_id = 21"
     )
     print("Rows Deleted:", result7)
     print("------------------------")
 
 # query8
-    result8 = db.select_statement(
-    selector="DELETE",
-    condition="is_expired = 'T'"
-    )
-    print("Rows Deleted:", result8)
-    print("------------------------")
-
-# query9
     result9 = db.select_statement(
     selector="SELECT",
-    column_name="file_id, created_date",
-    condition="is_expired = 'F' AND file_id < 100"
+    column_name="file_id, date_created",
+    condition="is_expired = 'F' AND file_id < 30"
     )
     print("Result 9:", result9)
     print("------------------------")
 
-# query10
+# query9
     result10 = db.select_statement(
     selector="SELECT",
     column_name="COUNT(*)",
